76630bedf1268cbfe78df34ec98ff0a1779d8234
Fle2x <dotsfam87@Mail.ru>
2020-10-06 13:23:05 +0500

    v0_1

---
+++ ijunior-lesson-8/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _gamePanels;
+
+    public void ShowPanel(int index)
+    {
+        _gamePanels[index].SetActive(true);
+    }
+
+    public void HidePanel(int index)
+    {
+        _gamePanels[index].SetActive(false);
+    }
+}
